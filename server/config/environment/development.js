4ea811c9863d33a2db7d1fdd9c17985ebbe693c1
temystocles <temystocles@productfitter.com>
2016-01-17 21:55:27 +0000

    connecction

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -5,7 +5,8 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://localhost/app-dev'
+      //uri: 'mongodb://localhost/app-dev'
+      uri: 'mongodb://temy:telteltel@ds049754.mongolab.com:49754/tel'
   },
 
   seedDB: true
--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -18,6 +18,7 @@ module.exports = {
     uri:    process.env.MONGOLAB_URI ||
             process.env.MONGOHQ_URL ||
             process.env.OPENSHIFT_MONGODB_DB_URL+process.env.OPENSHIFT_APP_NAME ||
-            'mongodb://localhost/app'
+            'mongodb://temys:telteltel@ds049754.mongolab.com:49754/tel'
+            //'mongodb://localhost/app'
   }
 };
